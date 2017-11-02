de47af72794d7e23b2291c7c238d1ae2ace0a43e
ojhew <ojhew@hotmail.com>
2017-11-02 15:21:17 -0400

    fix #3: get file md5 hash

---
+++ getmd5hash.py
@@ -0,0 +1,16 @@
+def getmd5hash(fc):
+    m = hashlib.md5()
+    m.update(fc)
+    print(m.hexdigest())
+    return m.hexdigest()
+
+def getmd5hash():
+    path = input("Enter file path: ")
+    try:
+        fo = open(path, "rb");
+        fstr = fo.read();
+        hashStr = getmd5hash(fstr)
+        print(hashStr);
+        fo.close();
+    except:
+        print("File not found")
